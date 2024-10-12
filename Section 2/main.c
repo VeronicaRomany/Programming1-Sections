e63780b053ad96462585b5dd2a0599d051c4f549
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2024-10-12 11:46:37 +0300

    Section 2 Examples

---
+++ Section 2/main.c
@@ -0,0 +1,24 @@
+//
+// Created by veror on 10/7/2024.
+//
+
+#include <stdio.h>
+
+int main(){
+    int i;
+    float f;
+    double d;
+
+    scanf("%d", &i);
+    scanf("%f", &f);
+    scanf("%lf", &d);
+
+    printf("i = %d\n", i);
+    printf("f = %f\n", f);
+    printf("d = %lf\n", d);
+
+    return 0;
+}
+
+
+
